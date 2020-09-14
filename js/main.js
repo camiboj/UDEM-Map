ba0a92c43fa36c18ea0e804438cbc5879825edf5
Camila Bojman <camiboj@gmail.com>
2020-09-14 18:21:51 -0300

    model: agrego psico

--- js/main.js
+++ js/main.js
@@ -6,78 +6,12 @@ function main(carrera) {
     $("#materias").empty();
     let filename, titulo, tituloShort, plan;
     switch (carrera) {
-        case 'informatica':
-            filename = 'data/informatica-1986.csv';
-            plan = 'Plan 1986 v2016';
-            titulo = 'Ingeniería en Informática';
-            tituloShort = 'Informática';
+        case 'psicologia':
+            filename = 'data/psicologia-UDEM.csv';
+            plan = 'Plan';
+            titulo = 'Psicología';
+            tituloShort = 'Psicología';
             break;
-        case 'sistemas':
-            filename = 'data/sistemas-1986.csv';
-            plan = 'Plan 1986 v2016';
-            titulo = 'Licenciatura en Análisis de Sistemas';
-            tituloShort = 'Sistemas';
-            break;
-        case 'electronica':
-            filename = 'data/electronica-2009.csv';
-            plan = 'Plan 2009 v2019';
-            titulo = 'Ingeniería Electrónica';
-            tituloShort = 'Electrónica';
-            break;
-        case 'quimica':
-            filename = 'data/quimica-1986.csv';
-            plan = 'Plan 1986 v2016';
-            titulo = 'Ingeniería Química';
-            tituloShort = 'Química';
-            break;
-        case 'civil':
-            filename = 'data/civil-2009.csv';
-            plan = 'Plan 2009 v2016';
-            titulo = 'Ingeniería Civil';
-            tituloShort = 'Civil';
-            break;
-        case 'alimentos':
-            filename = 'data/alimentos-2000.csv';
-            plan = 'Plan 2000 v2016';
-            titulo = 'Ingeniería de Alimentos';
-            tituloShort = 'Alimentos';
-            break;
-        case 'electricista':
-            filename = 'data/electricista-2009.csv';
-            plan = 'Plan 2009 v2016';
-            titulo = 'Ingeniería Electricista';
-            tituloShort = 'Electricista';
-            break;
-        case 'naval':
-            filename = 'data/naval-1986.csv';
-            plan = 'Plan 1986 v2016';
-            titulo = 'Ingeniería Naval y Mecánica';
-            tituloShort = 'Naval';
-            break;
-        case 'mecanica':
-            filename = 'data/mecanica-1986.csv';
-            plan = 'Plan 1986 v2016';
-            titulo = 'Ingeniería Mecánica';
-            tituloShort = 'Mecánica';
-            break;
-        case 'petroleo':
-            filename = 'data/petroleo-2015.csv';
-            plan = 'Plan 2015 v2016';
-            titulo = 'Ingeniería en Petróleo';
-            tituloShort = 'Petróleo';
-            break;        
-        case 'agrimensura':
-            filename = 'data/agrimensura-2006.csv';
-            plan = 'Plan 2006 v2016';
-            titulo = 'Ingeniería en Argimensura';
-            tituloShort = 'Agrimensura';
-            break;        
-        case 'industrial':
-            filename = 'data/industrial-2011.csv';
-            plan = 'Plan 2011 v2019';
-            titulo = 'Ingeniería Industrial';
-            tituloShort = 'Industrial';
-            break;        
     }
 
     $("#carrera-actual-long").text(titulo + ' | ' + plan);
