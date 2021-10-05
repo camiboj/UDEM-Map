6be416843114b0423c6a40b83cd444c65016ac17
camix <camiboj@gmail.com>
2021-10-05 19:22:11 +0100

    add psicologa and delete others

---
+++ src/data/psicologia-UDEM.js
@@ -0,0 +1,558 @@
+export const psicologia = [
+    {
+        id: "CBC",
+        materia: "AH RE QUE NO HABIA CBC",
+        creditos: 1,
+        categoria: "*CBC",
+        level: -2
+    },
+
+    {
+        id: "H0200",
+        materia: "Psicología General I",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 1
+    },
+    
+    {
+        id: "H0002",
+        materia: "Bases Biológicas y Neurológicas del Comp",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 1
+    },
+
+    {
+        id: "H0003",
+        materia: "Historia de la Psicología",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 1
+    },
+
+    {
+        id: "H0202",
+        materia: "Epistemología y lógica",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 1
+    },
+
+    {
+        id: "H0203",
+        materia: "Filosofía",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 1
+    },
+
+    {
+        id: "H0009",
+        materia: "Fundamentos de Sociología",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 1
+    },
+
+    {
+        id: "H0201",
+        materia: "Psicología General II",
+        creditos: 1,
+        correlativas: "H0200",
+        categoria: "Materias Obligatorias",
+        level: 2
+    },
+
+    {
+        id: "H0012",
+        materia: "Neuropsicología",
+        creditos: 1,
+        correlativas: "H0002",
+        categoria: "Materias Obligatorias",
+        level: 2
+    },
+
+    {
+        id: "H0004",
+        materia: "Teorías Psicológicas Contemporáneas",
+        creditos: 1,
+        correlativas: "H0003",
+        categoria: "Materias Obligatorias",
+        level: 2
+    },
+
+    {
+        id: "H0204",
+        materia: "Metodologia de las Ciencias",
+        creditos: 1,
+        correlativas: "H0202",
+        categoria: "Materias Obligatorias",
+        level: 2
+    },
+
+    {
+        id: "H0205",
+        materia: "Antropología",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 2
+    },
+
+    {
+        id: "H0013",
+        materia: "Psicología de la Personalidad",
+        creditos: 1,
+        correlativas: "H0200",
+        categoria: "Materias Obligatorias",
+        level: 2
+    },
+
+    {
+        id: "H0254",
+        materia: "Inglés I",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 2
+    },
+
+    {
+        id: "A0001",
+        materia: "1er año",
+        creditos: 0,
+        correlativas: "H0200-H0002-H0003-H0202-H0203-H0009-H0201-H0012-H0004-H0204-H0205-H0013-H0254",
+        categoria: "Materias Obligatorias",
+        level: 3
+    },
+
+    {
+        id: "H0228",
+        materia: "Psicoanálisis I",
+        creditos: 1,
+        correlativas: "H0201",
+        categoria: "Materias Obligatorias",
+        level: 4
+    },
+
+    {
+        id: "H0206",
+        materia: "Psicología del Desarrollo I",
+        creditos: 1,
+        correlativas: "H0201",
+        categoria: "Materias Obligatorias",
+        level: 4
+    },
+
+    {
+        id: "H0208",
+        materia: "Cultura y Subjetividad",
+        creditos: 1,
+        correlativas: "H0203",
+        categoria: "Materias Obligatorias",
+        level: 4
+    },
+
+    {
+        id: "H0209",
+        materia: "Lenguaje y Comunicación",
+        creditos: 1,
+        correlativas: "H0201",
+        categoria: "Materias Obligatorias",
+        level: 4
+    },
+
+    {
+        id: "H0010",
+        materia: "Psicología Social",
+        creditos: 1,
+        correlativas: "H0009",
+        categoria: "Materias Obligatorias",
+        level: 4
+    },
+
+    {
+        id: "H0229",
+        materia: "Psicopatología I",
+        creditos: 1,
+        correlativas: "H0201",
+        categoria: "Materias Obligatorias",
+        level: 5
+    },
+
+    {
+        id: "H0207",
+        materia: "Psicología del Desarrollo II",
+        creditos: 1,
+        correlativas: "H0206",
+        categoria: "Materias Obligatorias",
+        level: 5
+    },
+
+    {
+        id: "H0020",
+        materia: "Dinamica de Grupos",
+        creditos: 1,
+        correlativas: "H0009",
+        categoria: "Materias Obligatorias",
+        level: 5
+    },
+
+    {
+        id: "H0015",
+        materia: "Evaluación y Técnicas Psicológicas I",
+        creditos: 1,
+        correlativas: "H0201-H0206",
+        categoria: "Materias Obligatorias",
+        level: 5
+    },
+
+    {
+        id: "H0021",
+        materia: "Psicosociología Educacional",
+        creditos: 1,
+        correlativas: "H0009",
+        categoria: "Materias Obligatorias",
+        level: 5
+    },
+
+    {
+        id: "H0255",
+        materia: "Inglés II",
+        creditos: 1,
+        correlativas: "H0223",
+        categoria: "Materias Obligatorias",
+        level: 5
+    },
+
+    {
+        id: "A0002",
+        materia: "2do año",
+        creditos: 0,
+        correlativas: "H0228-H0206-H0208-H0209-H0010-H0229-H0207-H0020-H0015-H0255",
+        categoria: "Materias Obligatorias",
+        level: 6
+    },
+
+    {
+        id: "H0230",
+        materia: "Psicoanálisis II",
+        creditos: 1,
+        correlativas: "H0228-H0209",
+        categoria: "Materias Obligatorias",
+        level: 7
+    },
+
+    {
+        id: "H0018",
+        materia: "Evaluación y Téncnicas Psicológicas II",
+        creditos: 1,
+        correlativas: "H0015-H0228",
+        categoria: "Materias Obligatorias",
+        level: 7
+    },
+
+    {
+        id: "H0231",
+        materia: "Psicopatología II",
+        creditos: 1,
+        correlativas: "H0229",
+        categoria: "Materias Obligatorias",
+        level: 7
+    },
+
+    {
+        id: "H0014",
+        materia: "Técnicas de Abordaje e Intervención en Crisis",
+        creditos: 1,
+        correlativas: "H0229",
+        categoria: "Materias Obligatorias",
+        level: 7
+    },
+
+    {
+        id: "H0023",
+        materia: "Psicología Organizacional",
+        creditos: 1,
+        correlativas: "H0020",
+        categoria: "Materias Obligatorias",
+        level: 7
+    },
+
+    {
+        id: "H0025",
+        materia: "OPP I",
+        creditos: 1,
+        correlativas: "A0001-A0002",
+        categoria: "Materias Obligatorias",
+        level: 7
+    },
+
+    {
+        id: "E0003",
+        materia: "Materia Electiva(*)",
+        creditos: 1,
+        correlativas: "A0001-A0002",
+        categoria: "Materias Obligatorias",
+        level: 8
+    },
+
+    {
+        id: "H0022",
+        materia: "Psicología Jurídica",
+        creditos: 1,
+        correlativas: "H0009-H0205",
+        categoria: "Materias Obligatorias",
+        level: 8
+    },
+
+    {
+        id: "H0210",
+        materia: "Ética y Deodontología Profesional",
+        creditos: 1,
+        correlativas: "H0203-H0208",
+        categoria: "Materias Obligatorias",
+        level: 8
+    },
+
+    {
+        id: "H0019",
+        materia: "Evaluación y Técnicas Psicológicas III",
+        creditos: 1,
+        correlativas: "H0018",
+        categoria: "Materias Obligatorias",
+        level: 8
+    },
+
+    {
+        id: "A0003",
+        materia: "3er año",
+        creditos: 0,
+        correlativas: "H0230-H0018-H0231-H0014-H0023-H0025-H0021-H0022-H0210-H0028-H0019-E0003",
+        categoria: "Materias Obligatorias",
+        level: 9
+    },
+
+    {
+        id: "H0032",
+        materia: "OPP II",
+        creditos: 1,
+        correlativas: "H0025",
+        categoria: "Materias Obligatorias",
+        level: 10
+    },
+
+    {
+        id: "H0211",
+        materia: "Métodos y Técnicas Psicoterapéuticas I",
+        creditos: 1,
+        correlativas: "H0231",
+        categoria: "Materias Obligatorias",
+        level: 10
+    },
+
+    {
+        id: "H0035",
+        materia: "Psicoterapia Familiar",
+        creditos: 1,
+        correlativas: "H0231-H0020",
+        categoria: "Materias Obligatorias",
+        level: 10
+    },
+
+    {
+        id: "H0029",
+        materia: "Psicología Clínica de niños y adolescentes",
+        creditos: 1,
+        correlativas: "H0206-H0231",
+        categoria: "Materias Obligatorias",
+        level: 10
+    },
+
+    {
+        id: "H0033",
+        materia: "Orientación Vocacional y Ocupacional",
+        creditos: 1,
+        correlativas: "H0018",
+        categoria: "Materias Obligatorias",
+        level: 10
+    },
+
+    {
+        id: "H0213",
+        materia: "Psicología Preventiva y Salud Comunitaria I",
+        creditos: 1,
+        correlativas: "H0231",
+        categoria: "Materias Obligatorias",
+        level: 10
+    },
+
+    {
+        id: "H0212",
+        materia: "Métodos y Técnicas Psicoterapéuticas II",
+        creditos: 1,
+        correlativas: "H0211",
+        categoria: "Materias Obligatorias",
+        level: 10
+    },
+
+    {
+        id: "H0030",
+        materia: "Psicología Clínica de Adultos",
+        creditos: 1,
+        correlativas: "H0029",
+        categoria: "Materias Obligatorias",
+        level: 11
+    },
+
+    {
+        id: "H0232",
+        materia: "Práctica Profesional Supervisada I",
+        creditos: 1,
+        correlativas: "A0003",
+        categoria: "Materias Obligatorias",
+        level: 11
+    },
+
+    {
+        id: "H0214",
+        materia: "Psicología Preventiva y Salud Comunitaria II",
+        creditos: 1,
+        correlativas: "H0213",
+        categoria: "Materias Obligatorias",
+        level: 11
+    },
+
+    {
+        id: "H0104",
+        materia: "Computación",
+        creditos: 1,
+        categoria: "Materias Obligatorias",
+        level: 11
+    },
+
+    {
+        id: "A0004",
+        materia: "4to año",
+        creditos: 0,
+        correlativas: "H0211-H0029-H0033-H0213-H0212-H0030-H0232-H0032-H0035-H0104",
+        categoria: "Materias Obligatorias",
+        level: 12
+    },
+
+    {
+        id: "E0005",
+        materia: "Materia Electiva(*)",
+        creditos: 1,
+        correlativas: "A0003",
+        categoria: "Materias Obligatorias",
+        level: 13
+    },
+
+    {
+        id: "H0233",
+        materia: "Práctica Profesional Supervisada II",
+        creditos: 1,
+        correlativas: "H0232",
+        categoria: "Materias Obligatorias",
+        level: 13
+    },
+
+    {
+        id: "H0257",
+        materia: "Trabajo Integrador Final",
+        creditos: 1,
+        correlativas: "A0004",
+        categoria: "Materias Obligatorias",
+        level: 13
+    },
+
+    {
+        id: "A0005",
+        materia: "5to año",
+        creditos: 0,
+        correlativas: "E0005-H0233-H0257",
+        categoria: "Materias Obligatorias",
+        level: 14
+    },
+
+    {
+        id: "H0043",
+        materia: "Psicopedagogía en Educación Especial I",
+        creditos: 0,
+        correlativas: "E0003",
+        categoria: "Materias Electivas",
+        level: 15
+    },
+
+    {
+        id: "H0220",
+        materia: "Investigación de Mercados",
+        creditos: 0,
+        correlativas: "E0003",
+        categoria: "Materias Electivas",
+        level: 15
+    },
+
+    {
+        id: "H0053",
+        materia: "Tratamiento Psicopedagógico",
+        creditos: 0,
+        correlativas: "E0003",
+        categoria: "Materias Electivas",
+        level: 15
+    },
+
+    {
+        id: "H0222",
+        materia: "Psicología de la tercera Edad",
+        creditos: 0,
+        correlativas: "E0003",
+        categoria: "Materias Electivas",
+        level: 15
+    },
+
+    {
+        id: "H0218",
+        materia: "Comunicación",
+        creditos: 0,
+        correlativas: "E0003",
+        categoria: "Materias Electivas",
+        level: 15
+    },
+
+    {
+        id: "H0219",
+        materia: "Publicidad y promoción",
+        creditos: 0,
+        correlativas: "E0005",
+        categoria: "Materias Electivas",
+        level: 16
+    },
+
+    {
+        id: "H0217",
+        materia: "Introducción al derecho",
+        creditos: 0,
+        correlativas: "E0005",
+        categoria: "Materias Electivas",
+        level: 16
+    },
+
+    {
+        id: "H0225",
+        materia: "Introducción a la Psicología del Deporte",
+        creditos: 0,
+        correlativas: "E0005",
+        categoria: "Materias Electivas",
+        level: 16
+    },
+
+    {
+        id: "H0221",
+        materia: "Comportamiento del consumidor",
+        creditos: 0,
+        correlativas: "E0005",
+        categoria: "Materias Electivas",
+        level: 16
+    },
+]
